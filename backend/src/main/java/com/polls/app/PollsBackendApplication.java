16413f6fb65cacd5cfe0f1f26bca49813b042e34
denisflorin03 <denispasare11@gmail.com>
2025-09-17 15:42:23 +0300

    Added entities and database schema

--- backend/src/main/java/com/polls/app/PollsBackendApplication.java
+++ backend/src/main/java/com/polls/app/PollsBackendApplication.java
@@ -1,13 +1,11 @@
-package com.polls.polls_backend;
+package com.polls.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PollsBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PollsBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PollsBackendApplication.class, args);
+    }
 }
