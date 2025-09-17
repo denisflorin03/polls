b61eedc91671e4e157422fc8d207a646f012a3de
denisflorin03 <denispasare11@gmail.com>
2025-09-17 14:42:04 +0300

    Initial commit with Nx, frontend + backend

---
+++ backend/src/test/java/com/polls/polls_backend/PollsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polls.polls_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
